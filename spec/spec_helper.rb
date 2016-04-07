b5eb584f69d8c6c4004d8f4b399fcaa10bac4299
robb-broome <robb@teamvoice.com>
2016-04-07 11:10:39 -0500

    consolidate specs, consolidate fun

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@ require 'rubygems'
 require 'rspec'
 require 'pry'
 
-require_relative '../chapter1.rb'
+fi = Dir[File.expand_path('funs/*/*.rb')]
+fi.each {|file| require file}
 
 spec_dir = File.dirname(__FILE__)
 $LOAD_PATH.unshift spec_dir
