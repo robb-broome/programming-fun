334ec7619437222e3ac693dc6103ab5e27c78b21
robb-broome <robb@teamvoice.com>
2017-03-07 12:54:25 -0600

    coin game

---
+++ funs/games/coin_game.rb
@@ -0,0 +1,9 @@
+# The coin game is a game where a number of coins are put in a center "pot".
+# Two players take turns where they can pick up 1, 2 or 4 coins from the pot.
+# The winner is the player who takes the last coin(s) from the pot.
+# Write a function that, given the number of coins in the center and the order
+# of the players' turns, returns which player will win the game if they both
+# play with optimal strategy
+# Examples:
+# coin_game(1, "alice", "bob") returns "alice"
+# coin_game(3, "alice", "bob") returns "bob"
